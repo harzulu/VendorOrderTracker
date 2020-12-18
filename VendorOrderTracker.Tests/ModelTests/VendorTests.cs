48614dc3e5a09c5b48fb4935a8d977895fa01fc6
Benjamin McFarland <benrmcfarland@gmail.com>
2020-12-18 10:48:28 -0800

    Add vendor code to get first test failing

---
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void MakeVendor_CreateInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("name", "description");
+      string testing = "test";
+      Assert.AreEqual(typeof(Vendor), testing.GetType());
+    }
+  }
+}
--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -3,8 +3,26 @@ using System.Collections.Generic;
 
 namespace VendorOrderTracker.Models
 {
-  public class VendorOrderTracker
+  public class Vendor
   {
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<Order> Orders { get; set; }
+    private static List<Vendor> _instances = new List<Vendor> { };
+    public int Id { get; set; }
 
+    public Vendor(string name, string description)
+    {
+      Name = name;
+      Description = description;
+      Orders = new List<Order> { };
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }
