321511d239900313963d63e1007f4c15c05754b6
Benjamin McFarland <benrmcfarland@gmail.com>
2020-12-18 09:54:41 -0800

    Pass createOrder test

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -17,8 +17,8 @@ namespace VendorOrderTracker.Tests
     public void MakeOrder_CreateInstanceOfOrder_Order()
     {
       Order newOrder = new Order("title", "description", 12, "date");
-      string test = "string";
-      Assert.AreEqual(typeof(Order), test.GetType());
+      //string test = "string";
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
   }
 }
